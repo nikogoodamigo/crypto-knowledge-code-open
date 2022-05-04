096dad95b5ac27a42fc95c879e108a7331ff24db
Danny <dn.webdev@yahoo.com>
2022-05-04 21:57:33 +0200

    Update 1.1

--- app.js
+++ app.js
@@ -1,15 +1,276 @@
 const express = require("express");
 const https = require("https");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+// mongoose.connect("mongodb://localhost:27017/cryptoDB", {
+//   useUnifiedTopology: true,
+//   useNewUrlParser: true
+// });
+
+mongoose.connect("mongodb+srv://dan-admin:Test123!@cluster0.c4zsp.mongodb.net/cryptoDB", {useUnifiedTopology: true, useNewUrlParser: true});
+
+
+const overviewSchema = {
+  name: String,
+  description: String,
+  logo: String
+};
+
+const Overview = mongoose.model("Overview", overviewSchema);
+
+const articleSchema = {
+  name: String,
+  date: String,
+  photo: String,
+  content: String,
+  author: String
+};
+
+const Article = mongoose.model("Article", articleSchema);
+
+// const news1 = new Article({
+//   name: "XRP taking over the world",
+//   date: "27.09.2021",
+//   photo: "https://play-lh.googleusercontent.com/aCyq5_tBBCKcD5f4yuiE3kaNc1HDbPLA7Tq7PoEqBk1RVODSqJQUYpB_ekCrW23qnhw",
+//   content: "This is final, XRP takes over the banking system",
+//   author: "Daniel Nyikó"
+// });
+//
+// news1.save();
+
+// const xrp = new Overview({
+//     name: "XRP",
+//     description: "Opis XRP",
+//     logo: "fab fa-btc"
+//   });
+//
+// xrp.save();
+
 
 const app = express();
-
-app.use(express.static("public"));
-
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
-  res.sendFile(__dirname + "/index.html")
+  res.render("home");
 });
+
+app.get("/news", function(req, res){
+  Article.find({}, function(err, articles){
+    res.render("news", {
+      articles: articles,
+    });
+  });
+});
+
+app.get("/cryptos", function(req, res){
+  Overview.find({}, function(err, overviews){
+    res.render("cryptos", {
+      overviews: overviews
+    })
+  });
+});
+
+app.get("/prices", function(req, res){
+  res.render("prices");
+});
+
+app.get("/overview/:overviewId", function(req, res){
+  const requestedOverviewId = req.params.overviewId;
+
+  Overview.findOne({_id: requestedOverviewId}, function(err, overview){
+    res.render("overview",{
+      name: overview.name,
+      logo: overview.logo,
+      description: overview.description
+    });
+  });
+});
+
+app.get("/panel", function(req, res){
+  var i = 0;
+  var k = 0;
+
+  Overview.find({}, function(err,overviews){
+    Article.find({}, function(err, articles){
+
+      overviews.forEach(function(overview){
+        return i++;
+      });
+
+      articles.forEach(function(article){
+        return k++;
+      });
+
+      res.render("panel", {
+        numberOfCryptos: i,
+        numberOfArticles: k
+      });
+
+    });
+  });
+});
+
+app.get("/panel-crypto", function(req, res){
+  Overview.find({}, function(err, overviews){
+    res.render("panel-crypto", {
+      overviews: overviews
+    })
+  });
+});
+
+app.get("/panel-news", function(req, res){
+  Article.find({}, function(err, articles){
+    res.render("panel-news", {
+      articles: articles
+    })
+  });
+});
+
+app.get("/compose-crypto", function(req, res){
+  res.render("compose-crypto");
+});
+
+app.post("/compose-crypto", function(req, res) {
+
+  const cryptoName = req.body.cryptoName;
+  const cryptoDescription = req.body.cryptoDescription;
+  const cryptoLogo = req.body.cryptoLogo;
+
+  const overview = new Overview({
+    name: cryptoName,
+    description: cryptoDescription,
+    logo: cryptoLogo
+  });
+
+  overview.save(function(err){
+    if (!err) {
+      res.redirect("/compose-crypto");
+    };
+  });
+
+});
+
+app.get("/update/:overviewId", function(req, res){
+  const requestedOverviewId = req.params.overviewId;
+
+  Overview.findOne({_id: requestedOverviewId}, function(err, overview){
+    res.render("update", {
+      name: overview.name,
+      description: overview.description,
+      logo: overview.logo,
+      _id: overview._id
+    });
+  });
+});
+
+app.post("/update", function(req, res){
+  const cryptoName = req.body.cryptoName;
+  const cryptoDescription = req.body.cryptoDescription;
+  const cryptoLogo = req.body.cryptoLogo;
+  const requestedOverviewId = req.body.cryptoId;
+
+  Overview.updateOne({_id: requestedOverviewId}, { $set: {name: cryptoName, description: cryptoDescription, logo: cryptoLogo}}, function(err){
+    if(!err){
+      console.log(requestedOverviewId + " updated");
+      res.redirect("panel");
+    }
+  });
+});
+
+app.post("/delete-crypto", function(req, res){
+  const deletedItemId= req.body.button;
+  console.log(deletedItemId);
+
+  Overview.deleteOne({_id: deletedItemId}, function(err){
+      if(err){
+        console.log(err);
+      }
+      else{
+        console.log("Successfully deleted");
+        res.redirect("/panel-crypto");
+      }
+    });
+});
+
+app.get("/compose-news", function(req, res){
+  res.render("compose-news");
+});
+
+app.post("/compose-news", function(req, res) {
+
+  const articleAuthor = req.body.articleAuthor;
+  const articleName = req.body.articleName;
+  const articleContent = req.body.articleContent;
+  const articlePhoto = req.body.articlePhoto;
+  const articleDate = req.body.articleDate;
+
+  const article = new Article({
+    name: articleName,
+    date: articleDate,
+    photo: articlePhoto,
+    content: articleContent,
+    author: articleAuthor
+  });
+
+  article.save(function(err){
+    if (!err) {
+      res.redirect("/compose-news");
+    };
+  });
+
+});
+
+app.get("/update-news/:articleId", function(req, res){
+  const requestedArticleId = req.params.articleId;
+
+  Article.findOne({_id: requestedArticleId}, function(err, article){
+    res.render("update-news", {
+      author: article.author,
+      name: article.name,
+      content: article.content,
+      photo: article.photo,
+      date: article.date,
+      _id: article._id
+    });
+  });
+});
+
+app.post("/update-news", function(req, res){
+  const articleName = req.body.articleName;
+  const articleContent = req.body.articleContent;
+  const articlePhoto = req.body.articlePhoto;
+  const articleDate = req.body.articleDate;
+  const articleAuthor = req.body.articleAuthor;
+  const requestedArticleId = req.body.articleId;
+
+  Article.updateOne({_id: requestedArticleId}, { $set: {name: articleName, content: articleContent, photo: articlePhoto, date: articleDate, author: articleAuthor}}, function(err){
+    if(!err){
+      console.log(requestedArticleId + " updated");
+      res.redirect("panel-news");
+    }
+  });
+});
+
+
+app.post("/delete-news", function(req, res){
+  const deletedItemId= req.body.button;
+  console.log(deletedItemId);
+
+  Article.deleteOne({_id: deletedItemId}, function(err){
+      if(err){
+        console.log(err);
+      }
+      else{
+        console.log("Successfully deleted");
+        res.redirect("/panel-news");
+      }
+    });
+});
+
+
 
 app.listen(process.env.PORT || 3000, function(){
   console.log("Server is running on port 3000");
